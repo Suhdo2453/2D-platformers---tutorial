b2b079e4da33364ef094e3f83c7fa015308a80ba
Suhdo <dungvipnr@gmail.com>
2023-01-27 10:45:32 +0700

    feat: stun state enemy

--- Assets/Scripts/Enemies/State Machine/Entity.cs
+++ Assets/Scripts/Enemies/State Machine/Entity.cs
@@ -14,6 +14,8 @@ public class Entity : MonoBehaviour
     public Animator anim { get; private set; }
     public GameObject aliveGO { get; private set; }
     public AnimationToStateMachine atsm { get; private set; }
+    public int lastDamageDirection { get; private set; }
+
 
     [SerializeField]
     private Transform wallCheck;
@@ -24,11 +26,24 @@ public class Entity : MonoBehaviour
     [SerializeField]
     private Transform playerCheck;
 
+    [SerializeField]
+    private Transform groundCheck;
+
+    private float currentHealth;
+    private float currentStunResistance;
+    private float lastDamageTime;
+
+
     private Vector2 velocityWorksapce;
+
+    protected bool isStunned;
 
     public virtual void Start()
     {
         facingDirection = 1;
+        currentHealth = entityData.maxHealth;
+        currentStunResistance = entityData.stunResistance;
+
         aliveGO = transform.Find("Alive").gameObject;
         rb = aliveGO.GetComponent<Rigidbody2D>();
         anim = aliveGO.GetComponent<Animator>();
@@ -41,6 +56,11 @@ public class Entity : MonoBehaviour
     public virtual void Update()
     {
         stateMachine.currentState.LogicUpdate();
+
+        if(Time.time >= lastDamageTime + entityData.stunRecoveryTime)
+        {
+            ResetStunResistance();
+        }
     }
 
     public virtual void FixedUpdate()
@@ -55,6 +75,13 @@ public class Entity : MonoBehaviour
 
     }
 
+    public virtual void SetVelocity(float velocity, Vector2 angle, int direction)
+    {
+        angle.Normalize();
+        velocityWorksapce.Set(angle.x * velocity * direction, angle.y * velocity);
+        rb.velocity = velocityWorksapce;
+    }
+
     public virtual bool CheckWall()
     {
         return Physics2D.Raycast(wallCheck.position, aliveGO.transform.right, entityData.wallCheckDistance, entityData.whatIsGround);
@@ -64,6 +91,11 @@ public class Entity : MonoBehaviour
     {
         return Physics2D.Raycast(ledgeCheck.position, Vector2.down, entityData.ledgeCheckDistace, entityData.whatIsGround);
 
+    }
+
+    public virtual bool CheckGround()
+    {
+        return Physics2D.OverlapCircle(groundCheck.position, entityData.groundCheckRadius, entityData.whatIsGround);
     }
 
     public virtual bool CheckPlayerInMinAgroRange()
@@ -79,6 +111,42 @@ public class Entity : MonoBehaviour
     public virtual bool CheckPlayerInCloseRangeAction()
     {
         return Physics2D.Raycast(playerCheck.position, aliveGO.transform.right, entityData.closeRangeActionDistance, entityData.whatIsPlayer);
+    }
+
+    public virtual void DamageHop(float velocity)
+    {
+        velocityWorksapce.Set(rb.velocity.x, velocity);
+        rb.velocity = velocityWorksapce;
+    }
+
+    public virtual void ResetStunResistance()
+    {
+        isStunned = false;
+        currentStunResistance = entityData.stunResistance;
+    }
+
+    public virtual void Damage(AttackDetails attackDetails)
+    {
+        lastDamageTime = Time.time;
+
+        currentHealth -= attackDetails.damageAmount;
+        currentStunResistance -= attackDetails.stunDamageAmount;
+
+        DamageHop(entityData.damageHopSpeed);
+
+        if(attackDetails.position.x > aliveGO.transform.position.x)
+        {
+            lastDamageDirection = -1;
+        }
+        else
+        {
+            lastDamageDirection = 1;
+        }
+
+        if(currentStunResistance <= 0)
+        {
+            isStunned = true;
+        }
     }
 
     public virtual void Flip()
--- Assets/Scripts/Enemies/States/Data/D_Entity.cs
+++ Assets/Scripts/Enemies/States/Data/D_Entity.cs
@@ -5,8 +5,16 @@ using UnityEngine;
 [CreateAssetMenu(fileName ="newEntityData", menuName ="Data/Entity Data/Base Data")]
 public class D_Entity : ScriptableObject
 {
+    public float maxHealth = 30f;
+
+    public float damageHopSpeed = 3f;
+
     public float wallCheckDistance = 0.2f;
     public float ledgeCheckDistace = 0.4f;
+    public float groundCheckRadius = 0.3f;
+
+    public float stunResistance = 3f;
+    public float stunRecoveryTime = 2f;
 
     public float maxAgroDistance = 4f;
     public float minAgroDistance = 3f;
