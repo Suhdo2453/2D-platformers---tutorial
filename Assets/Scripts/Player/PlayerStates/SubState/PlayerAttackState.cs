7a65308fc8648813e6e919f56f9b93aa24bd4a19
Suhdo <dungvipnr@gmail.com>
2023-03-22 22:50:07 +0700

    feat: attack movement

--- Assets/Scripts/Player/PlayerStates/SubState/PlayerAttackState.cs
+++ Assets/Scripts/Player/PlayerStates/SubState/PlayerAttackState.cs
@@ -5,22 +5,38 @@ using UnityEngine;
 public class PlayerAttackState : PlayerAbilityState
 {
     private Weapon weapon;
-    
-    public PlayerAttackState(Player player, PlayerStateMachine stateMachine, PlayerData playerData, string animBoolName) : base(player, stateMachine, playerData, animBoolName)
+
+    private float velocityToSet;
+    private bool setVelocity;
+
+    public PlayerAttackState(Player player, PlayerStateMachine stateMachine, PlayerData playerData, string animBoolName)
+        : base(player, stateMachine, playerData, animBoolName)
     {
     }
 
     public override void Enter()
     {
         base.Enter();
+
+        setVelocity = false;
         
         weapon.EnterWeapon();
+    }
+
+    public override void LogicUpdate()
+    {
+        base.LogicUpdate();
+
+        if (setVelocity)
+        {
+            player.SetVelocityX(velocityToSet * player.FacingDirection);
+        }
     }
 
     public override void Exit()
     {
         base.Exit();
-        
+
         weapon.ExitWeapon();
     }
 
@@ -35,8 +51,16 @@ public class PlayerAttackState : PlayerAbilityState
     public override void AnimationFinishTrigger()
     {
         base.AnimationFinishTrigger();
-        
+
         isAbilityDone = true;
+    }
+
+    public void SetPlayerVelocity(float velocity)
+    {
+        player.SetVelocityX(velocity * player.FacingDirection);
+
+        velocityToSet = velocity;
+        setVelocity = true;
     }
 
     #endregion
--- Assets/Scripts/Weapons/Weapon.cs
+++ Assets/Scripts/Weapons/Weapon.cs
@@ -4,10 +4,14 @@ using UnityEngine;
 
 public class Weapon : MonoBehaviour
 {
+    [SerializeField] private SO_WeaponData weaponData;
+    
     protected Animator baseAnimator;
     protected Animator weaponAnimator;
 
     protected PlayerAttackState state;
+
+    protected int attackCounter;
 
     protected virtual void Start()
     {
@@ -20,15 +24,25 @@ public class Weapon : MonoBehaviour
     public virtual void EnterWeapon()
     {
         gameObject.SetActive(true);
+
+        if (attackCounter >= weaponData.movementSpeed.Length)
+        {
+            attackCounter = 0;
+        }
         
         baseAnimator.SetBool("attack", true);
         weaponAnimator.SetBool("attack", true);
+        
+        baseAnimator.SetInteger("attackCounter", attackCounter);
+        weaponAnimator.SetInteger("attackCounter", attackCounter);
     }
 
     public virtual void ExitWeapon()
     {
         baseAnimator.SetBool("attack", false);
         weaponAnimator.SetBool("attack", false);
+
+        attackCounter++;
         
         gameObject.SetActive(false);
     }
@@ -38,6 +52,16 @@ public class Weapon : MonoBehaviour
     public virtual void AnimationFinishTrigger()
     {
         state.AnimationFinishTrigger();
+    }
+
+    public virtual void AnimationStartMovementTrigger()
+    {
+        state.SetPlayerVelocity(weaponData.movementSpeed[attackCounter]);
+    }
+
+    public virtual void AnimationStopMovementTrigger()
+    {
+        state.SetPlayerVelocity(0f);
     }
 
     #endregion
